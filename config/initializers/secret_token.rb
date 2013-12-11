909b359cb21a97c4715ded92aff2073f4a1a5bfc
Jenyaya <jenyaya@online.ua>
2013-12-11 11:04:21 +0000

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snow::Application.config.secret_key_base = 'e39d2c4b11575f6afde88a523fecefa3c6c58f3685c42b31784a65f6dda2673e394a4b48d5c6de656319865cbacef581bef478098d530e2aa39ef578eca4051c'
